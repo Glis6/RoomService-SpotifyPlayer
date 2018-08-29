96f083ca2676e31b7047a272c4b244a4df0a57d0
Gilles Vercammen <gillesv@live.be>
2018-08-29 21:34:32 +0200

    Subscriptions removed

--- src/main/java/com/glis/io/network/NetworkConfiguration.java
+++ src/main/java/com/glis/io/network/NetworkConfiguration.java
@@ -4,14 +4,15 @@ import com.glis.DomainController;
 import com.glis.io.network.client.networktype.ClientBoth;
 import com.glis.io.network.client.networktype.ClientDownStream;
 import com.glis.io.network.client.networktype.ClientUpstream;
-import com.glis.io.network.codec.SubscribeMessageEncoder;
 import com.glis.io.network.input.library.MappedMessageLibrary;
 import com.glis.io.network.input.library.MessageLibrary;
-import com.glis.io.network.networktype.*;
+import com.glis.io.network.networktype.Both;
+import com.glis.io.network.networktype.ClientHandlerCustomNetworkTypeHandler;
+import com.glis.io.network.networktype.Downstream;
+import com.glis.io.network.networktype.Upstream;
 import com.glis.message.AccessTokenMessage;
 import com.glis.message.Message;
 import com.glis.message.PlaybackMessage;
-import io.netty.channel.ChannelHandlerContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -40,13 +41,7 @@ public class NetworkConfiguration {
 
     @Bean
     public Downstream downstream(final DomainController domainController, final MessageLibrary messageLibrary) {
-        return new ClientDownStream(new ClientHandlerCustomNetworkTypeHandler(domainController) {
-            @Override
-            public void doCustom(ChannelHandlerContext channelHandlerContext, LinkData linkData) {
-                super.doCustom(channelHandlerContext, linkData);
-                channelHandlerContext.pipeline().addLast(new SubscribeMessageEncoder());
-            }
-        }, messageLibrary);
+        return new ClientDownStream(new ClientHandlerCustomNetworkTypeHandler(domainController), messageLibrary);
     }
 
     @Bean
