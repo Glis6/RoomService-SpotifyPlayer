a02b1a38ccf215d1e8151bdd8392972f6bfb1dcd
Gilles Vercammen <gillesv@live.be>
2018-09-01 14:37:05 +0200

    Environment variables renamed.

--- src/main/java/com/glis/SpringConfiguration.java
+++ src/main/java/com/glis/SpringConfiguration.java
@@ -1,11 +1,17 @@
 package com.glis;
 
+import com.glis.io.network.client.networktype.ClientDownStream;
 import com.glis.io.network.input.InputHandlerConfiguration;
 import com.glis.io.network.input.InputHandlerLibrary;
 import com.glis.io.network.input.dispatcher.InputDispatcher;
 import com.glis.io.network.input.dispatcher.PriorityInputDispatcher;
 import com.glis.io.network.input.handlers.InputHandler;
-import com.glis.io.network.NetworkConfiguration;
+import com.glis.io.network.input.library.MappedMessageLibrary;
+import com.glis.io.network.input.library.MessageLibrary;
+import com.glis.io.network.networktype.ClientHandlerCustomNetworkTypeHandler;
+import com.glis.message.AccessTokenMessage;
+import com.glis.message.Message;
+import com.glis.message.PlaybackMessage;
 import com.glis.util.HandlerLibrary;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +22,6 @@ import org.springframework.context.annotation.Import;
  */
 @Configuration
 @Import({
-        NetworkConfiguration.class,
         InputHandlerConfiguration.class
 })
 public class SpringConfiguration {
@@ -33,5 +38,22 @@ public class SpringConfiguration {
     @Bean
     public InputDispatcher inputDispatcher(final HandlerLibrary<InputHandler, Object> handlerLibrary) {
         return new PriorityInputDispatcher(handlerLibrary);
+    }
+    @Bean
+    public Message[] networkMessages() {
+        return new Message[]{
+                new PlaybackMessage(),
+                new AccessTokenMessage()
+        };
+    }
+
+    @Bean
+    public MessageLibrary messageLibrary(Message... messages) {
+        return new MappedMessageLibrary(messages);
+    }
+
+    @Bean
+    public ClientDownStream downstream(final DomainController domainController, final MessageLibrary messageLibrary) {
+        return new ClientDownStream(new ClientHandlerCustomNetworkTypeHandler(domainController), messageLibrary);
     }
 }
--- src/main/java/com/glis/StartUp.java
+++ src/main/java/com/glis/StartUp.java
@@ -1,6 +1,7 @@
 package com.glis;
 
 import com.glis.io.network.client.ServerConnection;
+import com.glis.io.network.client.networktype.ClientDownStream;
 
 import java.util.Optional;
 
@@ -12,11 +13,14 @@ public class StartUp {
      * The starting point of the application.
      */
     public static void main(String[] args) throws Exception {
+
+        ApplicationContextProvider.getApplicationContext().getBean(ClientDownStream.class);
+
         try (ServerConnection serverConnection = new ServerConnection(
                 Optional.ofNullable(System.getenv("HOST")).orElseThrow(() -> new Exception("HOST is not set in the environment")),
                 Integer.parseInt(Optional.ofNullable(System.getenv("PORT")).orElseThrow(() -> new Exception("PORT is not set in the environment"))),
-                Optional.ofNullable(System.getenv("LEDCONTROLLER_CLIENTID")).orElseThrow(() -> new Exception("LEDCONTROLLER_CLIENTID is not set in the environment")),
-                Optional.ofNullable(System.getenv("LEDCONTROLLER_CLIENTSECRET")).orElseThrow(() -> new Exception("LEDCONTROLLER_CLIENTSECRET is not set in the environment"))
+                Optional.ofNullable(System.getenv("SPOTIFYPLAYER_CLIENTID")).orElseThrow(() -> new Exception("SPOTIFYPLAYER_CLIENTID is not set in the environment")),
+                Optional.ofNullable(System.getenv("SPOTIFYPLAYER_CLIENTSECRET")).orElseThrow(() -> new Exception("SPOTIFYPLAYER_CLIENTSECRET is not set in the environment"))
         )) {
             serverConnection.connect();
         }
